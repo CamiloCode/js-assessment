b712167b0c2b5b5a933662ca5a7afb61a0e980cd
Omar Contreras <ocontreras@alertlogic.com>
2016-03-16 16:57:42 -0500

    modules.js part fix

--- tests/app/modules.js
+++ tests/app/modules.js
@@ -4,9 +4,32 @@ if ( typeof window === 'undefined' ) {
 }
 
 describe('the module pattern', function() {
+
+  var modulesAnswers;
+
+  modulesAnswers = {
+    createModule: function(param1,param2) {
+    Module = (function(){
+            this.name = param2;
+            this.greeting = param1;
+
+            var sayIt = function() { 
+                        return greeting + ', ' + name;
+                    };
+ 
+            return {
+                sayIt: sayIt,
+                name: this.name,
+                greeting : this.greeting
+
+            };
+    })();
+      return Module;
+    }  
+  }
   it('you should be able to create a function that returns a module', function() {
     var module = modulesAnswers.createModule('hello', 'matt');
-
+    alert(console.log(module));
     expect(module.sayIt).to.be.a('function');
     expect(module.name).to.eql('matt');
     expect(module.greeting).to.eql('hello');
